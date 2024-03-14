b80485bf57fb42d3aef28c1a80a4347e6fa8e3da
DeVoyt <egorpomidorik532@gmail.com>
2024-03-14 19:58:33 +0600

    init

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:coffe_shop/src/app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const CoffeeShopApp());
+}
---
+++ lib/src/app.dart
@@ -0,0 +1,17 @@
+
+import 'package:coffe_shop/src/features/view/menu_screen.dart';
+import 'package:coffe_shop/src/theme/theme.dart';
+import 'package:flutter/material.dart';
+
+class CoffeeShopApp extends StatelessWidget {
+  const CoffeeShopApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: theme,
+      home: const MenuScreen(),
+    );
+  }
+}
